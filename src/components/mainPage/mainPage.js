81bcc9e6ac8feaa674566a349eeb39723b7cb027
Ivan <lipton2807@yandex.ru>
2023-09-08 12:00:49 +0300

    fix(): post last bug hot fix

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -5,74 +5,80 @@ import Card from "../card";
 import ChoosePanel from "../choosePanel";
 
 
-
 const exRate = {
-	"1":80,
-	"2":100,
-	"0":1,
+  "1": 80,
+  "2": 100,
+  "0": 1,
 };
 const PRECISION = 10_000;
 const mainPage = props => {
-	const renderCards = (data,sort) => {
-		if(!data.length){
-			return <Card data={null}></Card>
-		}
-		return data.sort((a,b)=> (!sortPrice) ? (a.price-b.price): b.price-a.price).map(el=>{ if(!el.payment) el['payment']='0' ; return el}).map(el => (<Card data={el}></Card>))}
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [sortPrice, setSortPrice] = React.useState(0)
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [stops, setStops] = React.useState(props.data.sort((a,b)=> b.stops - a.stops)[0].stops)
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	// const [sortDepature, setSortDepature] = React.useState(0)
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [data,setData]= React.useState(props.data)
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [filterStop,setFilterStop] = React.useState([])
-	const changePayment = (previousPayment, payment) =>{
-		if(!data || !data.length){
-			return
-		}
-		setData(props.data.map(el=> {
-			if (previousPayment === payment){
-				return el
-			}
-			if (previousPayment !== 0) {
-				const conversion = el.price * exRate[previousPayment];
-				el.price = (Math.round((conversion / exRate[payment]) * PRECISION) / PRECISION);
-			} else if (payment !== 0) {
-				const conversion = el.price / exRate[payment];
-				el.price = (Math.round((conversion / exRate[previousPayment]) * PRECISION) / PRECISION);
-			}
-			el['payment'] = payment
-			return el}))
-	}
+  const renderCards = (data, sort) => {
+    if (!data.length) {
+      return <Card data={null}></Card>
+    }
+    return data.sort((a, b) => (!sortPrice) ? (a.price - b.price) : b.price - a.price).map(el => {
+      if (!el.payment) el['payment'] = '0';
+      return el
+    }).map(el => (<Card data={el}></Card>))
+  }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [sortPrice, setSortPrice] = React.useState(0)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [stops, setStops] = React.useState(props.data.sort((a, b) => b.stops - a.stops)[0].stops)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  // const [sortDepature, setSortDepature] = React.useState(0)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [data, setData] = React.useState(props.data)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [filterStop, setFilterStop] = React.useState([])
+  const changePayment = (previousPayment, payment) => {
+    if (!data || !data.length) {
+      return
+    }
+    setData(data.map(el => {
+      if (previousPayment === payment) {
+        return el
+      }
+      if (previousPayment !== 0) {
+        const conversion = el.price * exRate[previousPayment];
+        el.price = (Math.round((conversion / exRate[payment]) * PRECISION) / PRECISION);
+      } else if (payment !== 0) {
+        const conversion = el.price / exRate[payment];
+        el.price = (Math.round((conversion / exRate[previousPayment]) * PRECISION) / PRECISION);
+      }
+      el['payment'] = payment
+      return el
+    }))
+  }
 
-	const filterStops = (stops) => {
+  const filterStops = (stops) => {
 
-				if(!stops || !stops.length){
-					setData(props.data)
-					return
-				}
-		const data = props.data.filter(el => {
-			return stops.filter(stop => stop === el.stops).length
-		})
-				setData(data)
+    if (!stops || !stops.length) {
+      setData(props.data)
+      return
+    }
+    const data = props.data.filter(el => {
+      return stops.filter(stop => stop === el.stops).length
+    })
+    setData(data)
+  }
 
-	}
+  return (
+    <div className={'page'}>
 
-	return (
-	<div className={'page'}>
+      <ChoosePanel changePayment={changePayment} stops={stops} filterStops={filterStops}></ChoosePanel>
+      <div className={'subpage'}>
+        <Button type={'text'} onClick={() => {
+          setSortPrice(+!sortPrice)
+        }}>{'Сортировать по цене ' + ((sortPrice) ? '↑' : '↓')}</Button>
+        {/*<Button type={'text'} onClick={() => {setSortDepature(+!sortDepature)}}>{'Сортировать по дате отправления '+ ((sortDepature) ? '↑':'↓')}</Button>*/}
+        <Space direction="vertical" size={16} className={'customSpace'}>
+          {renderCards(data, sortPrice,)}
+        </Space></div>
+    </div>
 
-		<ChoosePanel changePayment={changePayment} stops={stops} filterStops={filterStops}></ChoosePanel>
-		<div className={'subpage'}>
-			<Button type={'text'} onClick={() => {setSortPrice(+!sortPrice)}}>{'Сортировать по цене '+ ((sortPrice) ? '↑':'↓')}</Button>
-			{/*<Button type={'text'} onClick={() => {setSortDepature(+!sortDepature)}}>{'Сортировать по дате отправления '+ ((sortDepature) ? '↑':'↓')}</Button>*/}
-			<Space direction="vertical" size={16} className={'customSpace'}>
-				{renderCards(data, sortPrice, )}
-			</Space></div>
-</div>
-
-)};
+  )
+};
 
 // todo: Unless you need to use lifecycle methods or local state,
 // write your component in functional form as above and delete
@@ -84,7 +90,7 @@ const mainPage = props => {
 // }
 
 const mainPagePropTypes = {
-	// always use prop types!
+  // always use prop types!
 };
 
 mainPage.propTypes = mainPagePropTypes;
